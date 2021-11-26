035ac6ac4028e55d0163df0b6b2cd6fc58afe867
Northwemko <yinyangxiangsheng@gmail.com>
2021-11-26 16:00:31 +0800

    Files generated by PkgTemplates
    PkgTemplates version: 0.7.23

---
+++ src/UsefulFunctions.jl
@@ -0,0 +1,5 @@
+module UsefulFunctions
+
+# Write your package code here.
+
+end
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using UsefulFunctions
+using Test
+
+@testset "UsefulFunctions.jl" begin
+    # Write your tests here.
+end
